dec472423adc1aa77abbb91bc1d3e41f54dd775a
Martin Reinhardt <contact@martinreinhardt-online.de>
2016-02-10 07:13:51 +0100

    fix path resolve issue

--- test/parse.spec.js
+++ test/parse.spec.js
@@ -5,7 +5,7 @@ describe('parse', function () {
 
   it('should add live reload to application.js', function (done) {
     livereload.prepareApplicationJS(__dirname + '/application.js', 'http://localhost:9001', (result)=> {
-      expect(result).toEqual(`var livereload = require('livereload');
+      expect(result).toEqual(`var livereload = require('tvml-kit-livereload/lib/livereload');
 
 App.onLaunch = function () {
   liveReload.connect();
